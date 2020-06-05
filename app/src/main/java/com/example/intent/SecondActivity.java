dca45857b4527599e1942a23a8e93006f5bb9892
Luis Eduardo Carreón Soto <60121944+EduardoSo7o@users.noreply.github.com>
2020-06-05 16:38:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/intent/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void previousActivity(View view){
+        Intent intentNext = new Intent(this, MainActivity.class);
+        startActivity(intentNext);
+    }
+}
